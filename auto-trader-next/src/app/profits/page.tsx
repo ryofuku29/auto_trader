bd0384d1816dd2bb7b70b4d0aea18cabb0d1a27b
Ryosei Fukushima <ryosei-f@weseek.co.jp>
2025-07-23 02:51:48 +0900

    取引の合計収益を表示できる

--- auto-trader-next/src/app/profits/page.tsx
+++ auto-trader-next/src/app/profits/page.tsx
@@ -9,16 +9,28 @@ type Profit = {
 
 export default function ProfitsPage() {
   const [profits, setProfits] = useState<Profit[]>([]);
+  const [totalProfit, setTotalProfit] = useState<number>(0);
 
   useEffect(() => {
-    fetch("/api/profits")
-      .then((res) => res.json())
-      .then((data) => setProfits(data));
+    fetch("/api/profits") // fetch() は、Web API などに HTTP リクエストを送る関数。/api/profits に GET リクエストを送り、JSON を取得しようとしています。
+      .then((res) => res.json()) // fetchのresをjsonに変換。
+      .then((data: Profit[]) => {
+        setProfits(data); // 上の結果をdataという名前で受け取っている。
+        const total = data.reduce((sum, i) => sum + i.profit, 0); // array.reduce((累積結果, 今の要素) => { ... }, 初期値)
+        setTotalProfit(total);
+      });
   }, []);
 
   return (
     <main className="container mt-5">
       <h1 className="mb-4">利益</h1>
+      <h2>
+        合計収益：
+        <span className={totalProfit >= 0 ? "text-success" : "text-danger"}>
+          {totalProfit.toLocaleString()}円
+          {/* .toLocaleString()関数をつけることで、カンマがつき、見やすくなる。 */}
+        </span>
+      </h2>
       <table className="table table-borderer">
         <thead className="table-light">
           <tr>
